cfd2139b46f5dc82642a2f1c81d734a7506017a4
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-12-30 15:17:38 +0530

    LC

--- Memoize2.js
+++ Memoize2.js
@@ -35,3 +35,62 @@ Explanation:
 Merging two empty objects will always result in an empty object. The 2nd and 3rd third function calls result in a cache-hit. This is because every object passed in is identical.
 */
 
+
+class LookupTree {
+  map = new Map();
+  hasValue = false;
+  value = null;
+
+  getValueHelper(path, i) {
+    const key = path[i];
+    if (i >= path.length) {
+      if (this.hasValue) {
+        return { value: this.value, success: true };
+      } else {
+        return { value: undefined, success: false };
+      }
+    } else {
+      if (this.map.has(key)) {
+        return this.map.get(key).getValueHelper(path, i + 1);
+      } else {
+        return { value: undefined, success: false };
+      }
+    }
+  }
+
+  getValue(path) {
+    return this.getValueHelper(path, 0);
+  }
+
+  setValueHelper(path, i, value) {
+    const key = path[i];
+    if (i >= path.length) {
+      this.value = value;
+      this.hasValue = true;
+    } else {
+      if (!this.map.has(key)) {
+        this.map.set(key, new LookupTree());
+      }
+      return this.map.get(key).setValueHelper(path, i + 1, value);
+    }
+  }
+
+  setValue(path, value) {
+    return this.setValueHelper(path, 0, value);
+  }
+}
+
+function memoize(func) {
+  const tree = new LookupTree();
+  const newFunction = (...params) => {
+    const cache = tree.getValue(params);
+    if (cache.success) {
+      return cache.value;
+    }
+    const result = func(...params);
+    tree.setValue(params, result);
+    return result;
+  };
+  return newFunction;
+}
+
